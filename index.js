0690bf63cfd3470116aa0190cb5a70c5874b4c9c
SHALINI <76041100+Shalini-19-hub@users.noreply.github.com>
2021-01-23 19:57:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function viewsignup()
+    {
+
+    document.getElementById('signupform').style = 'visibility:visible';
+    document.getElementById('loginform').style = 'visibility:hidden';
+    document.getElementById('aboutus').style = 'visibility:hidden';
+
+
+    }
+
+function viewlogin()
+    {
+
+    document.getElementById('signupform').style = 'visibility:hidden';
+    document.getElementById('loginform').style = 'visibility:visible';
+    document.getElementById('aboutus').style = 'visibility:hidden';
+
+
+    }
